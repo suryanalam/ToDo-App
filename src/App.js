8fc446f0445a2797f8d3bd7c5f5ecea326e0cd94
surya <suryanalam3011@gmail.com>
2023-06-24 19:25:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [task, setTask] = useState('');
+  const [taskList, setTaskList] = useState([]);
+
+  const handleChange = (e) => {
+    if (e.target.checked) {
+      let updatedList = taskList.filter((item) => item !== e.target.value)
+      setTaskList(updatedList);
+      setTimeout(() => {alert(`Task: ${e.target.value} Completed`)}, 100);
+    }
+  }
+
+  const onSubmit = () => {
+      setTaskList([...taskList, task]);
+      setTask('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='heading'>TO-DO LIST</h1>
+      <form>
+        <input type="text" value={task} onChange={(e)=> setTask(e.target.value)} />
+        <button type="button" onClick={onSubmit}>Add</button>
+      </form>
+      <div  className='tasks-div'>
+        {taskList.map((item, index) =>
+          <div key={index} className='task'>
+            <input type='checkbox' key={index} value={item} name={item} className='checkbox' onChange={handleChange}/>
+            <label htmlFor={item} key={item} >{item}</label><br />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/Todo.js
@@ -0,0 +1,87 @@
+import './Todo.css';
+import React, { useState } from 'react';
+
+const Todo = () => {
+    const [items, setItems] = useState([]);
+    const [Task, setTask] = useState('');
+    const [editMode, setEditMode] = useState(false);
+    const [editItemId, setEditItemId] = useState(null);
+
+    const handleAddItem = () => {
+        const newItem = {
+            id: items.length + 1,
+            Task: Task,
+        };
+        setItems([...items, newItem]);
+        setTask('');
+    };
+
+    const handleEditItem = () => {
+        const updatedItems = items.map((item) => {
+            if (item.id === editItemId) {
+                return {
+                    id: item.id,
+                    Task: Task,
+                };
+            }
+            return item;
+        });
+
+        setItems(updatedItems);
+        setTask('');
+        setEditMode(false);
+        setEditItemId(null);
+    };
+
+    const handleDeleteItem = (id) => {
+        const updatedItems = items.filter((item) => item.id !== id);
+        setItems(updatedItems);
+    };
+
+    const handleEditTask = (id, Task) => {
+        setEditMode(true);
+        setEditItemId(id);
+        setTask(Task);
+    };
+
+    return (
+        <div className='bg'>
+
+            <div className='todo-bg'>
+               
+                <div className='task-header'>
+                    <h1>ToDo List</h1>
+                    <div>
+                        <input
+                            type="text"
+                            value={Task}
+                            onChange={(e) => setTask(e.target.value)}
+                            placeholder="Enter the task here..."
+                            required
+                        />
+                        <button onClick={editMode ? handleEditItem : handleAddItem} className='add-task-btn'>
+                            {editMode ? 'Save Changes' : 'Add Item'}
+                        </button>
+                    </div>
+                </div>
+
+                <div className='tasks-bg'>
+                    {items.map((item) => (
+                        <div key={item.id} className='task-details-div'>
+                            <div>{item.Task}</div>
+
+                            <div>
+                            <button onClick={() => handleEditTask(item.id, item.Task)}>Edit</button>{' '}
+                            <button onClick={() => handleDeleteItem(item.id)}>Delete</button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+        </div>
+
+    );
+};
+
+export default Todo;
